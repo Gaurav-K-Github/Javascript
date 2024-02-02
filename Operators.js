a88519ea7ba1cd49056ebbeb57a9f76dc1168785
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-02-02 20:07:36 +0530

    Add files via upload

---
+++ Operators.js
@@ -0,0 +1,8 @@
+console.log("Using operators:");
+console.log("\n");
+console.log("8+6=",8+6);
+console.log("8-6=",8-6);
+console.log("8*6=",8*6);
+console.log("8/6=",8/6);
+console.log("8%6=",8%6);
+console.log("8**6=",8**6);
