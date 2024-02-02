935a74e4cac777d01828246afdb9c4dc6b693727
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-02-02 20:08:46 +0530

    Update Operators.js

--- Operators.js
+++ Operators.js
@@ -2,3 +2,7 @@ console.log("Using operators:");
 console.log("\n");
 console.log("8+6=",8+6);
 console.log("8-6=",8-6);
+console.log("8*6=",8*6);
+console.log("8/6=",8/6);
+console.log("8%6=",8%6);
+console.log("8**6=",8**6);
